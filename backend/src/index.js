5bb06db99df4fff0af5c65908c1c4fdbc3380969
Aryan-Sameer <sameeraryan2005@gmail.com>
2025-07-30 17:41:25 +0530

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cookesParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import cors from "cors";
 import path from 'path';
 
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
-app.use(cookesParser());
+app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
